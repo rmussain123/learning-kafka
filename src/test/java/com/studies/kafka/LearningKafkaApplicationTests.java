f2f4dffe37580af1a7afbc20b75d4b4f47e3a858
Mohamed Ussain <49372437+rmussain123@users.noreply.github.com>
2025-02-23 11:58:33 +0530

    Initial

---
+++ src/test/java/com/studies/kafka/LearningKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studies.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
